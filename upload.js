f1148a0d859c93c308d596fa407c4260a2246257
Yanyi Lu <ylusharon@outlook.com>
2023-09-03 18:00:14 +0200

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,19 +1,29 @@
-// netlify/upload.js
+// fluffy/upload.js
 
 exports.handler = async function (event) {
-  try {
-    // Parse the form data from the event
-    // Process the form data as needed (e.g., save image, store data in a database)
-    // Return a response (e.g., success message or error)
+    try {
+        // Parse the form data from the event
+        const formData = new FormData(event.body);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Data received successfully' }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
+        // Extract form fields
+        const picture = formData.get('picture');
+        const buttonNumber = formData.get('button-number');
+        const email = formData.get('email');
+
+        // Process the form data as needed (e.g., save image, store data in a database)
+
+        // Return a JSON response indicating success
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Data received successfully' }),
+        };
+    } catch (error) {
+        // Handle any errors that may occur during processing
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
 };
+
+
